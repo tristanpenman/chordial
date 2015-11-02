0de077d7c0d8995cc820d583acd5cf88a6a7473f
Tristan Penman <tristan@tristanpenman.com>
2015-11-02 12:08:43 +1100

    Add placeholder case class for GetPredecessor operation and remove redundant case classes for error responses

--- modules/core/src/main/scala/com/tristanpenman/chordial/core/Node.scala
+++ modules/core/src/main/scala/com/tristanpenman/chordial/core/Node.scala
@@ -3,9 +3,12 @@ package com.tristanpenman.chordial.core
 import akka.actor._
 
 object NodeProtocol {
+  case class GetPredecessor()
+  case class GetPredecessorOk(predecessorId: Long, predecessorRef: ActorRef)
+  case class GetPredecessorOkButUnknown()
+
   case class GetSuccessor()
   case class GetSuccessorOk(successorId: Long, successorRef: ActorRef)
-  case class GetSuccessorError(message: String)
 
   case class Hello()
   case class HelloResponse()
@@ -18,14 +21,33 @@ object NodeProtocol {
 class Node(ownId: Long) extends Actor with ActorLogging {
   import NodeProtocol._
 
-  private def receiveWhileReady(successorId: Long, successorRef: ActorRef): Receive = {
+  private class NodeInfo(nodeId: Long, nodeRef: ActorRef) {
+    val id = nodeId
+    val ref = nodeRef
+  }
+
+  private object NodeInfo {
+    def apply(nodeId: Long, nodeRef: ActorRef) = new NodeInfo(nodeId, nodeRef)
+  }
+
+  private def receiveWhileReady(successor: NodeInfo, predecessor: Option[NodeInfo]): Receive = {
+    case GetPredecessor =>
+      predecessor match {
+        case Some(info) =>
+          sender() ! GetPredecessorOk(info.id, info.ref)
+        case None =>
+          sender() ! GetPredecessorOkButUnknown()
+      }
+
     case GetSuccessor =>
-      sender() ! GetSuccessorOk(successorId, successorRef)
+      sender() ! GetSuccessorOk(successor.id, successor.ref)
+
     case Hello =>
       sender() ! HelloResponse
+
     case Join(seed) =>
       sender() ! JoinError("Not implemented")
   }
 
-  override def receive = receiveWhileReady(ownId, self)
+  override def receive = receiveWhileReady(NodeInfo(ownId, self), None)
 }
