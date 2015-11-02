f6ca6e0e199d5081af249d89686a174d8cd4c226
Tristan Penman <tristan@tristanpenman.com>
2015-11-03 08:09:06 +1100

    Add support for asynchronous stabilisation

--- modules/core/src/main/scala/com/tristanpenman/chordial/core/Node.scala
+++ modules/core/src/main/scala/com/tristanpenman/chordial/core/Node.scala
@@ -1,24 +1,40 @@
 package com.tristanpenman.chordial.core
 
 import akka.actor._
+import akka.pattern.ask
+import akka.pattern.pipe
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 object NodeProtocol {
+
+  class GetPredecessorResponse
+
   case class GetPredecessor()
-  case class GetPredecessorOk(predecessorId: Long, predecessorRef: ActorRef)
-  case class GetPredecessorOkButUnknown()
+
+  case class GetPredecessorOk(predecessorId: Long, predecessorRef: ActorRef) extends GetPredecessorResponse
+
+  case class GetPredecessorOkButUnknown() extends GetPredecessorResponse
 
   case class GetSuccessor()
+
   case class GetSuccessorOk(successorId: Long, successorRef: ActorRef)
 
   case class Hello()
+
   case class HelloResponse()
 
   case class Join(seed: Option[ActorRef])
+
   case class JoinOk()
+
   case class JoinError(message: String)
+
 }
 
 class Node(ownId: Long) extends Actor with ActorLogging {
+
   import NodeProtocol._
 
   private class NodeInfo(nodeId: Long, nodeRef: ActorRef) {
@@ -30,7 +46,40 @@ class Node(ownId: Long) extends Actor with ActorLogging {
     def apply(nodeId: Long, nodeRef: ActorRef) = new NodeInfo(nodeId, nodeRef)
   }
 
-  private def receiveWhileReady(successor: NodeInfo, predecessor: Option[NodeInfo]): Receive = {
+  private case class BeginStabilisation()
+
+  private case class StabilisationFailed()
+
+  private case class UpdateSuccessor(successorId: Long, successorRef: ActorRef)
+
+  /** Time to wait between stabilisation attempts */
+  private val stabilisationInterval = Duration(2000, MILLISECONDS)
+
+  private val stabilisationTimeout = Duration(5000, MILLISECONDS)
+
+  /** Schedule periodic stabilisation */
+  context.system.scheduler.schedule(stabilisationInterval, stabilisationInterval, self, BeginStabilisation())
+
+  private def stabilise(successor: NodeInfo) = {
+    successor.ref.ask(GetPredecessor())(stabilisationTimeout)
+      .mapTo[GetPredecessorResponse]
+      .map({
+        case GetPredecessorOk(predecessorId: Long, predecessorRef: ActorRef) =>
+          UpdateSuccessor(successor.id, successor.ref)
+        case GetPredecessorOkButUnknown() =>
+          UpdateSuccessor(successor.id, successor.ref)
+      })
+      .recover({ case _ => StabilisationFailed() })
+      .pipeTo(self)
+  }
+
+  private def receiveWhileReady(successor: NodeInfo, predecessor: Option[NodeInfo], stabilising: Boolean): Receive = {
+    case BeginStabilisation() =>
+      if (!stabilising) {
+        context.become(receiveWhileReady(successor, predecessor, stabilising = true))
+        stabilise(successor)
+      }
+
     case GetPredecessor =>
       predecessor match {
         case Some(info) =>
@@ -47,7 +96,10 @@ class Node(ownId: Long) extends Actor with ActorLogging {
 
     case Join(seed) =>
       sender() ! JoinError("Not implemented")
+
+    case UpdateSuccessor(successorId: Long, successorRef: ActorRef) =>
+      context.become(receiveWhileReady(NodeInfo(successorId, successorRef), predecessor, stabilising = false))
   }
 
-  override def receive = receiveWhileReady(NodeInfo(ownId, self), None)
+  override def receive = receiveWhileReady(NodeInfo(ownId, self), None, stabilising = false)
 }
