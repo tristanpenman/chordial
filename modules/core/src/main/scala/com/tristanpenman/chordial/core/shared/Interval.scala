e701c441ac0cd367d735020e6af6787f4c35c89a
Tristan Penman <tristan@tristanpenman.com>
2015-11-21 08:57:54 +1100

    Fix bug in Interval contains() predicate

--- modules/core/src/main/scala/com/tristanpenman/chordial/core/shared/Interval.scala
+++ modules/core/src/main/scala/com/tristanpenman/chordial/core/shared/Interval.scala
@@ -2,7 +2,7 @@ package com.tristanpenman.chordial.core.shared
 
 class Interval(begin: Long, end: Long) {
   def contains(id: Long): Boolean =
-    if (begin < end) {
+    if (begin <= end) {
       id >= begin && id < end // Interval does not wrap around
     } else {
       id >= begin || id < end // Interval is empty (begin == end) or wraps back to Long.MinValue (begin > end)
