3e8975d4287d6f45d92ad2896cf0cd68dbd1484f
Tristan Penman <tristan@tristanpenman.com>
2015-11-21 14:02:14 +1100

    Remove ServiceActor

--- modules/daemon/src/main/scala/com/tristanpenman/chordial/daemon/service/ServiceActor.scala
+++
@@ -1,9 +0,0 @@
-package com.tristanpenman.chordial.daemon.service
-
-import akka.actor.{Actor, ActorContext, ActorRef}
-
-class ServiceActor(val nodeRef: ActorRef) extends Actor with Service {
-  def actorRefFactory: ActorContext = context
-
-  override def receive: Receive = runRoute(routes)
-}
