7af3308a67db2862a4c812fc073eb48042f62aec
Tristan Penman <tristan@tristanpenman.com>
2016-01-27 12:52:16 -0800

    Add unit test for POST requests on /nodes endpoint

--- modules/demo/src/test/scala/com/tristanpenman/chordial/demo/service/WebServiceSpec.scala
+++ modules/demo/src/test/scala/com/tristanpenman/chordial/demo/service/WebServiceSpec.scala
@@ -2,7 +2,7 @@ package com.tristanpenman.chordial.demo.service
 
 import akka.actor.{Actor, ActorRef, ActorSystem}
 import akka.testkit.TestActorRef
-import com.tristanpenman.chordial.demo.Governor.{GetNodeIdSet, GetNodeIdSetOk}
+import com.tristanpenman.chordial.demo.Governor.{CreateNodeOk, CreateNode, GetNodeIdSet, GetNodeIdSetOk}
 import com.tristanpenman.chordial.demo.WebService
 import org.scalatest.{ShouldMatchers, WordSpec}
 import spray.json._
@@ -14,8 +14,17 @@ class WebServiceSpec extends WordSpec with ShouldMatchers with WebService with S
 
   def actorRefFactory: ActorSystem = system
 
+  private def dummyActor: ActorRef = TestActorRef(new Actor {
+    def receive: Receive = {
+      case message =>
+        fail(s"Dummy actor should not receive any messages, but just received: ${message}")
+    }
+  })
+
   override protected def governor: ActorRef = TestActorRef(new Actor {
     def receive: Receive = {
+      case CreateNode() =>
+        sender() ! CreateNodeOk(1L, dummyActor)
       case GetNodeIdSet() =>
         sender() ! GetNodeIdSetOk(Set.empty)
     }
@@ -23,11 +32,20 @@ class WebServiceSpec extends WordSpec with ShouldMatchers with WebService with S
 
   "The web service" when {
     "backed by a Governor with no nodes" should {
-      "respond to GET requests on the /nodes endpoint with an empty JSON array" in {
+      "respond to a GET request on the /nodes endpoint with an empty JSON array" in {
         Get("/nodes") ~> routes ~> check {
           val jsonAst = responseAs[String].parseJson
           val jsonAsNodeAttrArray = jsonAst.convertTo[Iterable[NodeAttributes]]
           assert(jsonAsNodeAttrArray.isEmpty)
+        }
+      }
+      "respond to a POST request on the /nodes endpoint by returning an appropriate JSON object" in {
+        Post("/nodes") ~> routes ~> check {
+          val jsonAst = responseAs[String].parseJson
+          val jsonAsNodeAttr = jsonAst.convertTo[NodeAttributes]
+          assert(jsonAsNodeAttr.nodeId == 1L)
+          assert(jsonAsNodeAttr.successorId.contains(1L))
+          assert(jsonAsNodeAttr.active)
         }
       }
     }
