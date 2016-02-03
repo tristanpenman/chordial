2a3c1aa029dd438651f3efb7c66667f443db1f31
Tristan Penman <tristan@tristanpenman.com>
2016-02-02 22:26:43 -0800

    Add tests for CheckPredecessorAlgorithm

---
+++ modules/core/src/test/scala/com/tristanpenman/chordial/core/algorithms/CheckPredecessorAlgorithmSpec.scala
@@ -0,0 +1,117 @@
+package com.tristanpenman.chordial.core.algorithms
+
+import akka.actor.{ActorRef, Actor, ActorSystem}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import akka.util.Timeout
+import com.tristanpenman.chordial.core.Pointers._
+import com.tristanpenman.chordial.core.algorithms.CheckPredecessorAlgorithm._
+import com.tristanpenman.chordial.core.shared.NodeInfo
+import org.scalatest.WordSpecLike
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class CheckPredecessorAlgorithmSpec
+  extends TestKit(ActorSystem("CheckPredecessorAlgorithmSpec")) with WordSpecLike with ImplicitSender {
+
+  // Timeout for requests performed within CheckPredecessorAlgorithm actor
+  private val algorithmTimeout = Timeout(300.milliseconds)
+
+  // Time to wait before concluding that no additional messages will be received
+  private val spuriousMessageDuration = 150.milliseconds
+
+  // Function to create a new CheckPredecessorAlgorithm actor using a given ActorRef as the Pointers actor
+  private def newAlgorithmActor(pointersActor: ActorRef): ActorRef =
+    system.actorOf(CheckPredecessorAlgorithm.props(pointersActor, algorithmTimeout))
+
+  // Actor that will always discard messages
+  private val unresponsiveActor = TestActorRef(new Actor {
+    override def receive: Receive = {
+      case _ =>
+    }
+  })
+
+  "A CheckPredecessorAlgorithm actor" when {
+
+    "initialised with a Pointers actor that does not have a predecessor" should {
+      def newPointersActor: ActorRef = TestActorRef(new Actor {
+        override def receive: Receive = {
+          case GetPredecessor() =>
+            sender() ! GetPredecessorOkButUnknown()
+          case m =>
+            fail(s"Pointers actor received an unexpected message of type: ${m.getClass})")
+        }
+      })
+
+      "finish successfully without sending any further messages to the Pointers actor" in {
+        newAlgorithmActor(newPointersActor) ! CheckPredecessorAlgorithmStart()
+        expectMsg(CheckPredecessorAlgorithmFinished())
+        expectNoMsg(spuriousMessageDuration)
+      }
+    }
+
+    "initialised with a Pointers actor whose predecessor is healthy" should {
+      def newPointersActor: ActorRef = {
+        val healthyPredecessor = TestActorRef(new Actor {
+          override def receive: Receive = {
+            case GetSuccessorList() =>
+              sender() ! GetSuccessorListOk(NodeInfo(1L, self), List.empty)
+          }
+        })
+
+        TestActorRef(new Actor {
+          override def receive: Receive = {
+            case GetPredecessor() =>
+              sender() ! GetPredecessorOk(NodeInfo(1L, healthyPredecessor))
+            case m =>
+              fail(s"Pointers actor received an unexpected message of type: ${m.getClass})")
+          }
+        })
+      }
+
+      "finish successfully without sending any further messages to the Pointers actor" in {
+        newAlgorithmActor(newPointersActor) ! CheckPredecessorAlgorithmStart()
+        expectMsg(CheckPredecessorAlgorithmFinished())
+        expectNoMsg(spuriousMessageDuration)
+      }
+    }
+
+    "initialised with a Pointers actor whose predecessor is unresponsive" should {
+      def newPointersActor: ActorRef = TestActorRef(new Actor {
+        def receiveWithUnknownPredecessor: Receive = {
+          case GetPredecessor() =>
+            sender() ! GetPredecessorOkButUnknown()
+          case m =>
+            fail(s"Pointers actor received an unexpected message of type: ${m.getClass})")
+        }
+
+        override def receive: Receive = {
+          case GetPredecessor() =>
+            sender() ! GetPredecessorOk(NodeInfo(1L, unresponsiveActor))
+          case ResetPredecessor() =>
+            sender() ! ResetPredecessorOk()
+            context.become(receiveWithUnknownPredecessor)
+          case m =>
+            fail(s"Pointers actor received an unexpected message of type: ${m.getClass})")
+        }
+      })
+
+      "finish successfully after sending a ResetPredecessor message to the Pointer actor" in {
+        val pointersActor = newPointersActor
+        newAlgorithmActor(pointersActor) ! CheckPredecessorAlgorithmStart()
+        expectMsg(CheckPredecessorAlgorithmFinished())
+        expectNoMsg(spuriousMessageDuration)
+        pointersActor ! GetPredecessor()
+        expectMsg(GetPredecessorOkButUnknown())
+      }
+    }
+
+    "initialised with a Pointers actor that is unresponsive" should {
+      "finish with an error" in {
+        newAlgorithmActor(unresponsiveActor) ! CheckPredecessorAlgorithmStart()
+        expectMsgType[CheckPredecessorAlgorithmError]
+        expectNoMsg(spuriousMessageDuration)
+      }
+    }
+  }
+}
