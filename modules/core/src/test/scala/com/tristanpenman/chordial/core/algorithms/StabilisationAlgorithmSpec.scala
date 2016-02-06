bdbdbeb697aae1acea2d9c92ab06f1347fee1cf9
Tristan Penman <tristan@tristanpenman.com>
2016-02-06 13:09:25 -0800

    Add test case to StabilisationAlgorithmSpec to test behaviour when two successors have failed

--- modules/core/src/test/scala/com/tristanpenman/chordial/core/algorithms/StabilisationAlgorithmSpec.scala
+++ modules/core/src/test/scala/com/tristanpenman/chordial/core/algorithms/StabilisationAlgorithmSpec.scala
@@ -6,7 +6,7 @@ import akka.util.Timeout
 import com.tristanpenman.chordial.core.Node.{Notify, NotifyOk}
 import com.tristanpenman.chordial.core.Pointers
 import com.tristanpenman.chordial.core.Pointers._
-import com.tristanpenman.chordial.core.algorithms.StabilisationAlgorithm.{StabilisationAlgorithmFinished, StabilisationAlgorithmStart}
+import com.tristanpenman.chordial.core.algorithms.StabilisationAlgorithm._
 import com.tristanpenman.chordial.core.shared.NodeInfo
 import org.scalatest.WordSpecLike
 
@@ -31,6 +31,13 @@ class StabilisationAlgorithmSpec
     def receive: Receive = {
       case message =>
         fail(s"Dummy actor should not receive any messages, but just received: $message")
+    }
+  })
+
+  // Actor that will always discard messages
+  private def newUnresponsiveActor = TestActorRef(new Actor {
+    override def receive: Receive = {
+      case _ =>
     }
   })
 
@@ -62,17 +69,66 @@ class StabilisationAlgorithmSpec
       }
     }
 
+    "initialised with a Pointers actor with two successors, where both Nodes are failing to respond to " +
+      "GetPredecessor messages" should {
+      "finish successfully after removing both nodes from the Pointers actor's successor list" in {
+        // Failure in nodes 2 and 3 is simulated using actors that will discard all messages that are received
+        val node2 = newUnresponsiveActor
+        val node3 = newUnresponsiveActor
+
+        val testProbeForNode1 = new TestProbe(system)
+
+        // The Pointers actor, for node ID 1, that will be modified in response to failure
+        val pointersRef = TestActorRef(Pointers.props(1L, keyspaceBits, NodeInfo(2L, node2), system.eventStream))
+        pointersRef ! UpdatePredecessor(NodeInfo(3L, node3))
+        expectMsg(UpdatePredecessorOk())
+        val backupSuccessors = List(NodeInfo(3L, node3), NodeInfo(1L, testProbeForNode1.ref))
+        pointersRef ! UpdateSuccessorList(NodeInfo(2L, node2), backupSuccessors)
+        expectMsg(UpdateSuccessorListOk())
+
+        // Test probe to ensure that the correct messages are sent to Pointers actor
+        val pointersTestProbe = new TestProbe(system)
+
+        val algorithm = system.actorOf(
+          StabilisationAlgorithm.props(NodeInfo(1L, testProbeForNode1.ref), pointersTestProbe.ref, algorithmTimeout))
+        algorithm ! StabilisationAlgorithmStart()
+
+        // StabilisationAlgorithm actor should ask Pointers actor for current successor list
+        pointersTestProbe.expectMsg(GetSuccessorList())
+        pointersTestProbe.forward(pointersRef)
+
+        // After node 2 fails to respond to a GetPredecessor message, StabilisationActor should ask Pointers actor to
+        // remove node 2 from the its successor list
+        pointersTestProbe.expectMsg(
+          UpdateSuccessorList(NodeInfo(3L, node3), List(NodeInfo(1L, testProbeForNode1.ref))))
+        pointersTestProbe.forward(pointersRef)
+
+        // After node 3 fails to respond to a GetPredecessor message, StabilisationActor should ask Pointers actor to
+        // remove node 3 from the its successor list
+        pointersTestProbe.expectMsg(
+          UpdateSuccessorList(NodeInfo(1L, testProbeForNode1.ref), List.empty))
+        pointersTestProbe.forward(pointersRef)
+
+        // Assuming that node 1 does not have a predecessor...
+        testProbeForNode1.expectMsg(GetPredecessor())
+        testProbeForNode1.reply(GetPredecessorOkButUnknown())
+
+        // the StabilisationAlgorithm actor should send a Notify message to the node
+        testProbeForNode1.expectMsg(Notify(1L, testProbeForNode1.ref))
+        testProbeForNode1.reply(NotifyOk())
+
+        expectMsg(StabilisationAlgorithmFinished())
+        expectNoMsg(spuriousMessageDuration)
+        pointersTestProbe.expectNoMsg(spuriousMessageDuration)
+        testProbeForNode1.expectNoMsg(spuriousMessageDuration)
+      }
+    }
+
     "initialised with a Pointers actor with two successors, where the first node is failing to respond to " +
       "GetPredecessor messages" should {
       "finish successfully after removing the failed node from the Pointers actor's successor list" in {
-        val node2 = TestActorRef(new Actor {
-          override def receive: Receive = {
-            case GetPredecessor() =>
-              // Ignored to simulate failure
-            case m =>
-              fail(s"Stub Node actor for ID 2 received an unexpected message of type: ${m.getClass})")
-          }
-        })
+        // Failure in node 2 is simulated using an actor that will discard all messages that it receives
+        val node2 = newUnresponsiveActor
 
         val node3 = TestActorRef(new Actor {
           override def receive: Receive = {
