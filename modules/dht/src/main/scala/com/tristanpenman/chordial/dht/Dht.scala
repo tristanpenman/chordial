fdd44cbf9b4b0a5bde76904e8e5cf08b71fe66ef
Tristan Penman <tristan@tristanpenman.com>
2020-06-11 21:22:35 +1000

    Basic command line argument handling

--- modules/dht/src/main/scala/com/tristanpenman/chordial/dht/Dht.scala
+++ modules/dht/src/main/scala/com/tristanpenman/chordial/dht/Dht.scala
@@ -1,5 +1,7 @@
 package com.tristanpenman.chordial.dht
 
+import java.util.Properties
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
@@ -7,14 +9,56 @@ import akka.http.scaladsl.server.Directives.{get, handleWebSocketMessages, path}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Flow
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives.cors
+import com.typesafe.config.ConfigFactory
 import spray.json.DefaultJsonProtocol
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 object Dht extends App with DefaultJsonProtocol {
-  implicit val system = ActorSystem("Service")
+  val usage = """
+    Usage: dht [--seed-node address] [--virtual-nodes num]
+  """
+
+  if (args.length == 0) {
+    println(usage)
+    System.exit(0)
+  }
+
+  type OptionMap = Map[Symbol, Any]
+
+  @scala.annotation.tailrec
+  def consumeOptions(map: OptionMap, list: List[String]): OptionMap =
+    list match {
+      case Nil => map
+      case "--dht-port" :: value :: tail =>
+        consumeOptions(map ++ Map(Symbol("dht-port") -> value), tail)
+      case "--seed-node" :: value :: tail =>
+        consumeOptions(map ++ Map(Symbol("seed-node") -> value), tail)
+      case "--virtual-nodes" :: value :: tail =>
+        consumeOptions(map ++ Map(Symbol("virtual-nodes") -> value.toInt), tail)
+      case option :: _ =>
+        println("Unknown option " + option)
+        throw new Exception("Unknown option " + option)
+    }
+
+  // Parse command line arguments
+  val options = consumeOptions(Map(), args.toList)
+  println(options)
+
+  // Override default akka configuration
+  val properties = new Properties()
+  options.get(Symbol("dht-port")) match {
+    case Some(x: String) =>
+      properties.setProperty("akka.remote.netty.tcp.port", x)
+    case _ =>
+  }
+
+  // Set up an actor system for the DHT backend
+  var config = ConfigFactory.parseProperties(properties)
+  implicit val system = ActorSystem("Service", config.withFallback(ConfigFactory.load()))
   implicit val mat = ActorMaterializer()
+
   implicit val ec = system.dispatcher
 
   val dhtService: Flow[Message, Message, _] = Flow[Message].map {
